733a3453ba91624754e3659534c69a966392f66e
ximo <ximomonfort@gmail.com>
2025-12-12 11:51:55 +0100

    Añade estructura inicial del proyecto, dependencias y .gitignore

--- Chuletarium/src/views/Home.jsx
+++ Chuletarium/src/views/Home.jsx
@@ -1,9 +1,19 @@
-// src/views/Home.jsx
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Chuletarium</h1>
-      <p>Bienvenido a tu web de chuletas de programación 😎</p>
-    </main>
+    <div ClassName="page">
+      <Header />  {/* Con <Header /> pintamos os cambios del archivo Header.jsx */}
+
+      <main>
+        <Hero /> {/* Con <Hero /> pintamos os cambios del archivo Hero.jsx */}
+
+        <section classname="container">
+          <h1>Chuletarium</h1>
+          <p>Bienvenido a tu web de chuletas de programación 😎</p>
+        </section>
+      </main>
+    </div>
   );
 }
